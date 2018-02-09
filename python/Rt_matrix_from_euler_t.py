dd93111535f3a9242c830cd5424aa8b8eb65efd1
hu <cocohuyue@gmail.com>
2018-02-09 13:56:35 +0100

    commit

--- python/Rt_matrix_from_euler_t.py
+++ python/Rt_matrix_from_euler_t.py
@@ -9,6 +9,16 @@ import numpy as np
 from math import cos, sin
 
 def R_matrix_from_euler_t(alpha,beta,gamma):
+  """
+  A=BCD. Z -> X -> Z
+  1. the first rotation is by an angle phi about the z-axis using D,
+  2. the second rotation is by an angle theta in [0,pi] about the former x-axis (now x^') using C, and
+  3. the third rotation is by an angle psi about the former z-axis (now z^') using B.
+  :param alpha:
+  :param beta:
+  :param gamma:
+  :return:
+  """
   R = np.eye(4, dtype=np.float32)
   R[0,0]=cos(alpha)*cos(gamma)-cos(beta)*sin(alpha)*sin(gamma)
   R[1,0]=cos(gamma)*sin(alpha)+cos(alpha)*cos(beta)*sin(gamma)
