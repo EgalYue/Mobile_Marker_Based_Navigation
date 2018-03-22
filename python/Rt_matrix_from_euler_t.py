418ece92ea181e92dc379ff540f43e68286ffb33
hu <cocohuyue@gmail.com>
2018-03-22 22:35:03 +0100

    fix error: Proper Euler angles, intrinsic rotations

--- python/Rt_matrix_from_euler_t.py
+++ python/Rt_matrix_from_euler_t.py
@@ -10,27 +10,28 @@ from math import cos, sin
 
 def R_matrix_from_euler_t(alpha,beta,gamma):
   """
+  https://en.wikipedia.org/wiki/Euler_angles
   A=BCD. Z -> X -> Z
-  1. the first rotation is by an angle phi about the z-axis using D,
+  1. the first rotation is by an angle phi about the z-axis using B,
   2. the second rotation is by an angle theta in [0,pi] about the former x-axis (now x^') using C, and
-  3. the third rotation is by an angle psi about the former z-axis (now z^') using B.
+  3. the third rotation is by an angle psi about the former z-axis (now z^') using D.
   :param alpha:
   :param beta:
   :param gamma:
   :return:
   """
   R = np.eye(4, dtype=np.float32)
-  R[0,0]=cos(alpha)*cos(gamma)-cos(beta)*sin(alpha)*sin(gamma)
-  R[1,0]=cos(gamma)*sin(alpha)+cos(alpha)*cos(beta)*sin(gamma)
-  R[2,0]=sin(beta)*sin(gamma)
+  R[0,0] = cos(alpha)*cos(gamma) - cos(beta)*sin(alpha)*sin(gamma)
+  R[0,1] = -cos(alpha)*sin(gamma) - cos(beta)*cos(gamma)*sin(alpha)
+  R[0,2] = sin(alpha)*sin(beta)
 
-  R[0,1]=-cos(beta)*cos(gamma)*sin(alpha)-cos(alpha)*sin(gamma)
-  R[1,1]=cos(alpha)*cos(beta)*cos(gamma)-sin(alpha)*sin(gamma)
-  R[2,1]=cos(gamma)*sin(beta)
+  R[1,0] = cos(gamma)*sin(alpha) + cos(alpha)*cos(beta)*sin(gamma)
+  R[1,1] = cos(alpha)*cos(beta)*cos(gamma) - sin(alpha)*sin(gamma)
+  R[1,2] = -cos(alpha)*sin(beta)
 
-  R[0,2]=sin(alpha)*sin(beta)
-  R[1,2]=-cos(alpha)*sin(beta)
-  R[2,2]=cos(beta)
+  R[2,0] = sin(beta)*sin(gamma)
+  R[2,1] = cos(gamma)*sin(beta)
+  R[2,2] = cos(beta)
 
   return R
 
