bce1ef8a17bc37cecd348b88f94d5229de56d5de
hu <cocohuyue@gmail.com>
2018-03-24 20:11:00 +0100

    accuracy functioon

--- python/CondNum_TheoryValidation/accuracy_function.py
+++ python/CondNum_TheoryValidation/accuracy_function.py
@@ -14,17 +14,34 @@ from vision.camera import Camera
 from vision.camera_distribution import create_cam_distribution,create_cam_distribution_in_YZ,create_cam_distribution_in_XZ
 import brute_force as bf
 
-# TODO add parameter : marker-size; camera intrinsic parameter
-def accuracy_func(accuracy_matrix, theta_params = (0.0,180.0,5), r_params = (0.0, 2.0, 5) , z = 0, y = 0):
+def accuracy_func(plane_size=(0.3, 0.3),camera_intrinsic_params=(800.,800.,320.,240.), theta_params=(0.0, 180.0, 5), r_params=(0.1, 2.0, 5), z=0, y=0):
     """
-    Calculate the accuracy degree of given position y,z in world 
-    
-    y: 
-    z: 
+    Calculate the accuracy degree of given position y,z in world
+
+    y:
+    z:
     :return 0 means the cam is out of range of marker
     """
-    degree = 5 # divide accuracy into 5 degree
-    r = np.sqrt(z*z + y*y)
+
+    # --------------------------------------accuracy_degree_distribution---------------------
+    cam = Camera()
+    fx = camera_intrinsic_params[0]
+    fy = camera_intrinsic_params[1]
+    cx = camera_intrinsic_params[2]
+    cy = camera_intrinsic_params[3]
+    cam.set_K(fx=fx, fy=fy, cx=cx, cy=cy)
+    width = cx * 2
+    height = cy * 2
+    cam.set_width_heigth(width, height)
+
+    cams,accuracy_mat = create_cam_distribution_in_YZ(cam=None, plane_size=plane_size, theta_params=theta_params, r_params=r_params,
+                                  plot=False)
+    inputX, inputY, inputZ, input_ippe1_t, input_ippe1_R, input_ippe2_t, input_ippe2_R, input_pnp_t, input_pnp_R, input_transfer_error, display_mat,accuracy_mat_new = bf.heightGetCondNum(cams,accuracy_mat,theta_params, r_params)
+
+    # print accuracy_mat_new
+    # --------------------------------------------------------------------------------------------
+    degree = 5  # divide accuracy into 5 degree
+    r = np.sqrt(z * z + y * y)
     # out of range
     if r >= r_params[1] or r == 0:
         return 0
@@ -38,7 +55,7 @@ def accuracy_func(accuracy_matrix, theta_params = (0.0,180.0,5), r_params = (0.0
         half_r_step = r_step / 2.0
         r_num1 = math.floor(r / r_step)
         r_num2 = math.floor((r - r_step * r_num1) / half_r_step)
-        row_index = int(r_num1 + r_num2) # row index
+        row_index = int(r_num1 + r_num2)  # row index
 
         angle_begin = theta_params[0]
         angle_end = theta_params[1]
@@ -46,51 +63,20 @@ def accuracy_func(accuracy_matrix, theta_params = (0.0,180.0,5), r_params = (0.0
         angle_step = (angle_end - angle_begin) / (angle_num - 1)
         # print "angle_step",angle_step
         half_angle_step = angle_step / 2.0
-        angle = np.rad2deg(np.arccos(y / r)) # deg
+        angle = np.rad2deg(np.arccos(y / r))  # deg
         angle_num1 = math.floor(angle / angle_step)
         angle_num2 = math.floor((angle - angle_step * angle_num1) / half_angle_step)
         col_index = int(angle_num1 + angle_num2)
         # print "row_index,col_index",row_index,col_index
-        acc_degree = accuracy_matrix[row_index, col_index]
+        acc_degree = accuracy_mat_new[row_index, col_index]
         # print "acc_degree",acc_degree
         return acc_degree
 
-def accuracy_degree_distribution():
-    """
-    Using a matrix to describe accuracy degree
-    :return: an accuracy matrix for detection area of marker  
-    """
-    # TODO Change the radius of circular plane
-    cam = Camera()
-    cam.set_K(fx=800, fy=800, cx=640 / 2., cy=480 / 2.)
-    cam.set_width_heigth(640, 480)
-    angle_begin = 0.0
-    angle_end = 180.0
-    angle_num = 4
-    angle_step = (angle_end - angle_begin) / (angle_num - 1)
-    theta_params = (angle_begin,angle_end,angle_num)
-
-    r_begin = 0.0
-    r_end = 2.0
-    r_num = 5
-    r_step = (r_end - r_begin) / (r_num - 1)
-    r_params = (r_begin,r_end,r_num)
-    # TODO cam distribution position PARAMETER CHANGE!!!
-    cams,accuracy_mat = create_cam_distribution_in_YZ(cam=None, plane_size=(0.3, 0.3), theta_params=theta_params, r_params=r_params,
-                                  plot=False)
-    inputX, inputY, inputZ, input_ippe1_t, input_ippe1_R, input_ippe2_t, input_ippe2_R, input_pnp_t, input_pnp_R, input_transfer_error, display_mat,accuracy_mat_new = bf.heightGetCondNum(cams,accuracy_mat,r_step,angle_step)
-    # print "accuracy_mat distribution:\n",accuracy_mat_new
-    return accuracy_mat_new
-
 
 # =========================================Test=========================================================
-accuracy_mat_new = accuracy_degree_distribution()
-print "accuracy_mat_new",accuracy_mat_new
-print "result: ",accuracy_func(accuracy_mat_new, theta_params = (0.0,180.0,5), r_params = (0.0, 2.0, 5) , z = 1, y = 0)
 
-#=========================================Test==========================================================
-# mat = np.eye(20,36)
-# accuracy_func(mat,0.1,0.1)
+# print accuracy_func(plane_size=(0.3, 0.3),camera_intrinsic_params=(800.,800.,320.,240.), theta_params=(0.0, 180.0, 5), r_params=(0.1, 2.0, 5), z=1, y=0)
+
 
 
 
